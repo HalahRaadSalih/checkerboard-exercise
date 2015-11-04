043a9c280a07f8a8454addb12b62e67c2127ef0d
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-11-04 13:56:36 -0800

    added implemtation of random colors

--- script.js
+++ script.js
@@ -30,16 +30,17 @@ function appendSquare(square){
 
 function createCheckeredBoard(){
 	for (var i = 0; i< 99; i++){
-		if(i%2 === 0){
-			appendSquare(createSquare('black','11.1%'));
-
-		}
-		else{
-			appendSquare(createSquare('red','11.1%'));
-
-		}
+			appendSquare(createSquare(generateRandomColor(),'11.1%'));
 	}
 }
 
+function generateRandomColor(){
+	var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color.toString();
+}
 
 createCheckeredBoard();
