884e02d2d0620770b3cc98eb0d836e14611be224
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-11-04 13:46:56 -0800

    added implementation for Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 // Your JS goes here
+
+function createSquare(color, size){
+
+	var square = document.createElement('div');
+
+	sizeAndPositonSaqure(square, size,size);
+ 	colorSquare(square, color);
+
+ 	return square;
+}
+
+function sizeAndPositonSaqure(square, size, padding){
+	square.style.width = size;
+ 	square.style.height = size;
+ 	square.style.float = 'left';
+ 	square.style.paddingBottom = size;
+}
+
+function colorSquare(square, color){
+	 square.style.background = color;
+
+}
+
+function appendSquare(square){
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(square);
+}
+
+
+function createCheckeredBoard(){
+	for (var i = 0; i< 99; i++){
+		if(i%2 === 0){
+			appendSquare(createSquare('black','11.1%'));
+
+		}
+		else{
+			appendSquare(createSquare('red','11.1%'));
+
+		}
+	}
+}
+
+
+createCheckeredBoard();
